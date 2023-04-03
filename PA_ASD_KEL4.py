ebc37dc26f7d8e9e510caab9840d9920a90fba2a
Taufik-Hidayat21 <taufikcoyhidayat@gmail.com>
2023-04-03 10:42:07 +0700

    tugas PA

---
+++ PA_ASD_KEL4.py
@@ -0,0 +1,8 @@
+import  mysql.connector 
+from prettytable import PrettyTable
+mydb = mysql.connector.connect(
+    host= "sql12.freesqldatabase.com",
+    user="sql12609741",
+    password="X6QKXcWMWz",
+    database="sql12609741"
+)

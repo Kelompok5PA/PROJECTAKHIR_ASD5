c9d3c1a5103d79a0e7ae40990346f7ddcb594d13
Taufik-Hidayat21 <122289679+Taufik-Hidayat21@users.noreply.github.com>
2023-04-10 14:57:06 +0700

    ganti menjadi kerupuk

--- PA_ASD_KEL4.py
+++ PA_ASD_KEL4.py
@@ -9,18 +9,16 @@ mydb = mysql.connector.connect(
     password="taufik123",
     database="login_menu"
 )
-class Node:
-    def __init__(self):
-        self.next = None
- 
-class LinkedList:
+class Toko:
     def __init__(self):
         self.head = None
-        self.laptop = ["Asus","Acer","Lenovo","Xiaomi"]
-        self.harga = [8000000,10000000,6000000,4000000]
+        self.kerupuk = ["Kerupuk Kulit","Kerupuk Udang","Kerupuk Bawang","Amplang"]
+        self.harga = [15000,30000,10000,25000]
         self.stok = [10,5,15,20]
+        self.expire = ["10-07-2024","11-09-2024","21-10-2024","28-12-2024"]
         self.sortedharga = []
         self.sortedstok= []
+        self.sortedexpire = []
         self.saldoakun = 0
         self.databeli = {
             "namabarang" : [],
@@ -32,18 +30,22 @@ class LinkedList:
         self.head = None
         self.harga.clear()
         self.stok.clear()
+        self.expire.clear()
 
-    def append1(self,sortedharga,sortedstok):
+    def append1(self,sortedharga,sortedstok,sortedexpire):
         self.head = None
         for i in range(len(sortedharga)):
             self.harga.append(self.sortedharga[i])
         for i in range(len(sortedstok)):
             self.stok.append(self.sortedstok[i])
+        for i in range(len(sortedexpire)):
+            self.expire.append(self.sortedexpire[i])
 
-    def update(self,index,namabaru,hargabaru,stokbaru):
-        self.laptop[index-1] = namabaru
+    def update(self,index,namabaru,hargabaru,stokbaru,expirebaru):
+        self.kerupuk[index-1] = namabaru
         self.harga[index-1] = hargabaru
         self.stok[index-1] = stokbaru
+        self.expire[index-1] = expirebaru
 
     def jumpSearch(self, arr , x , n ):
         step = math.sqrt(n)
@@ -89,20 +91,20 @@ class LinkedList:
             current_index += 1
         return -1
     def print1(self):
-        if len(self.laptop) == 0 :
+        if len(self.kerupuk) == 0 :
             print("Linked list kosong")
         else:
             table = PrettyTable()
-            table.field_names = ["No","Nama Laptop","Harga","Stok"]
+            table.field_names = ["No","Nama Kerupuk","Harga","Stok","Expire"]
             x1 = 1
-            for c in range(len(self.laptop)):
-                table.add_row([x1,self.laptop[c],self.harga[c],self.stok[c]])
+            for c in range(len(self.kerupuk)):
+                table.add_row([x1,self.kerupuk[c],self.harga[c],self.stok[c],self.expire[c]])
                 x1 += 1
             print(table)   
     def printsaldo(self):
         print(f"Saldo Anda : {self.saldoakun}") 
 
-link1 = LinkedList()
+link1 = Toko()
 
 def create_account(mydb):
     cursor = mydb.cursor()
@@ -135,21 +137,22 @@ def user_login():
         link1.saldoakun += result[0]
         print("Login berhasil. Selamat datang, {}!".format(user[1]))
         while True:
+            time.sleep(1)
             print('''
             Menu Yang Tersedia
-            1.Beli Barang
-            2.Lihat Barang
+            1.Beli Kerupuk
+            2.Lihat Kerupuk
             3.Informasi Saldo
             4.Tambah Saldo
-            5.Struk Barang
+            5.Struk Pembelian
             6.Keluar Menu
             ''')
             inputuser = input('Masukan menu yang diinginkan: ')
             if inputuser == "1":
                 while True:
                     link1.print1()
-                    inputbarang = int(input("Masukan Nomor Barang: "))
-                    inputjumlah = int(input("Masukan Jumlah Barang: "))
+                    inputbarang = int(input("Masukan Nomor Kerupuk: "))
+                    inputjumlah = int(input("Masukan Jumlah Kerupuk: "))
                     if inputjumlah > link1.stok[inputbarang-1] or inputjumlah < 1 :
                         print("Input Salah")
                     else:
@@ -158,7 +161,7 @@ def user_login():
                     kurangisaldo = link1.saldoakun - link1.harga[inputbarang-1]*inputjumlah
                     link1.saldoakun = kurangisaldo
                     link1.stok[inputbarang-1] = link1.stok[inputbarang-1] - inputjumlah
-                    link1.databeli["namabarang"].append(link1.laptop[inputbarang-1])
+                    link1.databeli["namabarang"].append(link1.kerupuk[inputbarang-1])
                     link1.databeli["hargabarang"].append(link1.harga[inputbarang-1])
                     link1.databeli["jumlahbarang"].append(inputjumlah)
                     link1.databeli["totalharga"].append(link1.harga[inputbarang-1]*inputjumlah)
@@ -180,14 +183,14 @@ def user_login():
                     print(f'Saldo telah tertambah menjadi: {link1.saldoakun}')
             elif inputuser == "5":
                 tabel1 = PrettyTable()
-                tabel1.field_names = ["No","Nama Barang","Harga Barang","Jumlah Barang","Total Harga"]
+                tabel1.field_names = ["No","Nama Kerupuk","Harga Kerupuk","Jumlah Barang","Total Harga"]
                 no=1
                 for i in range(len(link1.databeli.get("namabarang"))):
                     tabel1.add_row([no, link1.databeli.get("namabarang")[i],link1.databeli.get("hargabarang")[i],link1.databeli.get("jumlahbarang")[i],link1.databeli.get("totalharga")[i]])
                     no+=1
                 print(tabel1)
                 print("Total Semua Pembelian Anda : ",sum(link1.databeli.get("totalharga")))
-                with open('invoicelaptop.txt', 'w') as w:
+                with open('invoicekerupuk.txt', 'w') as w:
                     w.write(str(tabel1))
                     w.write(str("\n"))
                     w.write(str("Total Semua Pembelian Anda : "))
@@ -195,7 +198,7 @@ def user_login():
             elif inputuser == "6":
                  break
             else:
-                print("input salah")
+                 print("input menu salah")
     else:
                 print("Username dan/atau Password salah")
                 time.sleep(2)
@@ -216,6 +219,7 @@ def admin_login():
     # Jika ditemukan admin dengan username dan password yang sesuai
     if admin:
         print("Login berhasil. Selamat datang, {}!".format(admin[1]))
+        time.sleep(1)
         while True:
             print('''
             Menu Yang Tersedia
@@ -230,31 +234,35 @@ def admin_login():
             inputadmin = input('Masukan menu yang diinginkan: ')
             if inputadmin == "1":
                             while True:
-                                input1 = input("Masukan Nama Laptop: ")
+                                input1 = input("Masukan Nama kerupuk: ")
                                 if "\t" not in input1:
                                     break
                                 else:                            
                                     print("Masukan input dengan benar")
                                 input2 = int(input("Masukan harga: "))
                                 input3 = int(input("Masukan Stok: "))
-                                link1.laptop.append(input1)
+                                input4 = input("Masukan tanggal expire (xx-xx-xxxx): ")
+                                link1.kerupuk.append(input1)
                                 link1.harga.append(input2)
                                 link1.stok.append(input3)
+                                link1.expire.append(input4)
             elif inputadmin == "2":
                 link1.print1()
             elif inputadmin == "3":
-                index_lst = list(range(len(link1.laptop)))
-                link1.shell_sort(link1.laptop,index_lst)
+                index_lst = list(range(len(link1.kerupuk)))
+                link1.shell_sort(link1.kerupuk,index_lst)
                 for i in index_lst:
                     link1.sortedharga.append(link1.harga[i])
                 for i in index_lst:
                     link1.sortedstok.append(link1.stok[i])
+                for i in index_lst:
+                    link1.sortedexpire.append(link1.expire[i])
                 link1.clear_data()   
-                link1.append1(link1.sortedharga,link1.sortedstok)
+                link1.append1(link1.sortedharga,link1.sortedstok,link1.sortedexpire)
             elif inputadmin == "4":
                             link1.print1()
                             inputsearch = input('Masukan nama: ')
-                            qq = link1.jumpSearch(link1.laptop,inputsearch,len(link1.laptop))
+                            qq = link1.jumpSearch(link1.kerupuk,inputsearch,len(link1.kerupuk))
                             if qq == -1:
                                 print('data tidak ada')
                             else:
@@ -263,29 +271,31 @@ def admin_login():
                 while True:
                     link1.print1()
                     inputhapus = int(input("Masukan No Yang Ingin Dihapus: "))
-                    if inputhapus > len(link1.laptop-1) or inputhapus < 1:
+                    if inputhapus > len(link1.kerupuk-1) or inputhapus < 1:
                             print("input salah")
                     else:
                             break
-                link1.laptop.pop[inputhapus-1]
+                link1.kerupuk.pop[inputhapus-1]
                 link1.harga.pop[inputhapus-1]
                 link1.stok.pop[inputhapus-1]
+                link1.expire.pop[inputhapus-1]
             elif inputadmin == "6":
                 while True:
                     link1.print1()
                     inputupdate = int(input("Masukan No Yang Ingin Diupdate: "))
-                    if inputupdate > len(link1.laptop)-1 or inputupdate < 1:
+                    if inputupdate > len(link1.kerupuk)-1 or inputupdate < 1:
                         print("input salah")
                     else:
                         break
                 namabaru = input('Masukan nama baru: ')
                 hargabaru = int(input("Masukan harga baru: "))
                 stokbaru = int(input('Masukan stok baru: '))
-                link1.update(inputupdate,namabaru,hargabaru,stokbaru)
+                expirebaru = input("Masukan tanggal expire (xx-xx-xxxx): ")
+                link1.update(inputupdate,namabaru,hargabaru,stokbaru,expirebaru)
             elif inputadmin == "7":
-                break
+                            break
             else:
-                print("Input Menu Salah")
+                            print("Input Menu Salah")
     else:
         print("Username dan/atau Password salah")
         time.sleep(2)
@@ -293,6 +303,7 @@ def admin_login():
 # Main program
 def login():
     while True:
+        time.sleep(1)
         print("Selamat datang di program login.")
         print("Pilih opsi:")
         print("1. Regis User")
